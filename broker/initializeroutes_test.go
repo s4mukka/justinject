576e039343b1da6637b5b4e1fec7a5b5490194f7
s4mukka <meninosam197@gmail.com>
2024-07-29 15:24:21 -0300

    test: test Job Service

--- broker/initializeroutes_test.go
+++ broker/initializeroutes_test.go
@@ -20,3 +20,26 @@ func TestIntializeRoutes(t *testing.T) {
 	assert.Equal(t, 200, w.Code)
 	assert.Equal(t, `"pong"`, w.Body.String())
 }
+
+func exampleHandler(ctx IRestContext) {
+	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
+}
+
+func TestRoute(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	router := gin.New()
+	router.GET("/test", route(exampleHandler))
+
+	req, err := http.NewRequest(http.MethodGet, "/test", nil)
+	assert.NoError(t, err)
+
+	w := httptest.NewRecorder()
+
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	expectedBody := `{"message":"success"}`
+	assert.JSONEq(t, expectedBody, w.Body.String())
+}
